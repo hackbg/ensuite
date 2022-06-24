e1397e2696239c2cf875f28df35179bbf3c946b8
Adam A <adam@hack.bg>
2022-06-24 20:16:58 +0300

    feat: add entrypoint

---
+++ ensuite.cjs
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const ganesha = require.resolve('@hackbg/ganesha/index.js')
+const ensuite = require.resolve('./ensuite.js')
+require('@hackbg/ganesha').main([process.argv[0], ganesha, ensuite, ...process.argv.slice(2)])
---
+++ ensuite.js
@@ -0,0 +1,25 @@
+import why from 'why-is-node-still-running'
+import { runSpec } from '@hackbg/runspec'
+import { resolve} from 'path'
+main(...process.argv.slice(2))
+export async function main (root, ...specs) {
+
+  // If tests don't exit, press "?" to see why
+  process.stdin.setRawMode(true)
+  process.stdin.resume()
+  process.stdin.setEncoding('utf8')
+  process.stdin.on('data', key => {
+    if (key === '\u0003' || key === '\u0004') {
+      process.exit(100)
+    }
+    if (key === '?') {
+      why.whyIsNodeStillRunning()
+    }
+  })
+
+  // Run tests
+  const index = resolve(process.cwd(), root)
+  const suite = await import(index)
+  await runSpec(suite.default, specs)
+
+}
