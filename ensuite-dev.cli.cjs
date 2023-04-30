02db0bf73d23e0cb29ef34d4266f4eb7e87bbd73
Adam Avramov <adam.b.avramov@gmail.com>
2023-04-30 12:04:53 +0200

    feat(dev): rewrite pages specified in ensuite.yml

--- ensuite-dev.cli.cjs
+++ ensuite-dev.cli.cjs
@@ -4,39 +4,41 @@ const { readFileSync } = require('fs')
 const { join, sep, resolve } = require('path')
 const { createServer } = require('http')
 const { statSync } = require('fs')
+const { load } = require('js-yaml')
 
 if (require.main === module) main({}, process.argv.slice(2))
-
-module.exports = Object.assign(main, {
-  handle,
-  wsHandle,
-})
+module.exports = Object.assign(main, { handle, wsHandle, })
 
 /** Launch the Ensuite development server. */
 function main (state, args) {
   // Parse arguments
-  const [ port = 1234, index = '/index.pug' ] = args || []
+  const [ port = 1234, root = process.cwd(), config = resolve(root, 'ensuite.yml') ] = args || []
   // Init state
-  state = state ?? {}
+  state.root   ||= root
+  state.port   ||= port
+  state.config ||= load(readFileSync(config, 'utf8'))
   // Create HTTP server
-  if (!state.server) {
-    state.server = state.server || require('http')
-      .createServer((req, res)=>require(__filename).handle(req, res))
-      .listen(port, ()=>console.info(`Listening on`, port))
-  }
-  // Create WebSocker server
-  if (!state.wsServer) {
-    state.wsServer = new (require('ws').WebSocketServer)({ server: state.server })
+  state.server ||= require('http')
+    .createServer((req, res)=>require(__filename).handle(state, req, res))
+    .listen(port, ()=>console.info(`Listening on`, port))
+  // Create WebSocket server
+  state.wsServer ||= new (require('ws').WebSocketServer)({ server: state.server })
     state.wsServer.on('listening', ()=>console.info(`WebSocket listening on`, port))
     state.wsServer.on('connection', (ws, req)=>require(__filename).wsHandle(ws, req))
-  }
   // Return mutated state
   return state
 }
 
 /** Handle a HTTP request. */
-async function handle (req, res) {
+async function handle (state, req, res) {
   const url = new URL(req.url, `http://${req.headers.host}`)
+  console.log(req.method, url.pathname)
+  for (const { path, page } of state.config.routes) {
+    if (url.pathname === path) {
+      console.log('Matched', page)
+      url.pathname = page
+    }
+  }
   try {
     const handler = await import('./ensuite-render.cli.mjs')
     const data = await handler.render(url)
