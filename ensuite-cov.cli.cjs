89e693b031ed16b2540efc134189332a749d48d0
Adam Avramov <adam.b.avramov@gmail.com>
2023-05-03 21:35:27 +0200

    fix(1.4.3): run coverage with missing config

--- ensuite-cov.cli.cjs
+++ ensuite-cov.cli.cjs
@@ -11,11 +11,16 @@ const [
   ? [ [], argv ]                      // no double dash, all args to test
   : [ argv.slice(0, doubleDash), argv.slice(doubleDash + 1) ]
 
+let config
+try {
+  config = require('js-yaml').load(require('fs').readFileSync('ensuite.yml', 'utf8'))
+} catch (e) {
+  console.warn(`Failed to load ensuite.yml (${e.code})`)
+}
+
+const { coverage: { exclude = [] } = {} } = config || {}
+
 argvCov.unshift('--all')
-
-const { coverage: { exclude = [] } = {} } =
-  require('js-yaml').load(require('fs').readFileSync('ensuite.yml', 'utf8'))
-
 for (const path of exclude) {
   argvCov.push('--exclude')
   argvCov.push(JSON.stringify(path))
