cb1b6594533b0642f2fd104ed2e9315dae095e8b
Adam Avramov <adam@hack.bg>
2023-07-20 15:29:48 +0300

    fix: logs, mime, navigator

--- ensuite-dev.cli.cjs
+++ ensuite-dev.cli.cjs
@@ -6,11 +6,14 @@ const { createServer } = require('http')
 const { statSync } = require('fs')
 const { load } = require('js-yaml')
 
+const { Console } = require('@hackbg/logs')
+const console = new Console('ensuite-dev')
+
 if (require.main === module) main({}, process.argv.slice(2))
 module.exports = Object.assign(main, { handle, wsHandle, })
 
 /** Launch the Ensuite development server. */
-function main (state, args) {
+async function main (state, args) {
   // Parse arguments
   const [ port = 1234, root = process.cwd(), config = resolve(root, 'ensuite.yml') ] = args || []
   // Init state
@@ -42,7 +45,10 @@ async function handle (state, req, res) {
   try {
     const { renderUrl } = await import('./ensuite-render.cli.mjs')
     const data = await renderUrl(state, url)
-    const mime = url.pathname.endsWith('.svg') ? 'image/svg+xml' : 'text/html'
+    const mime =
+      url.pathname.endsWith('.svg') ? 'image/svg+xml' :
+      url.pathname.endsWith('.mjs') ? 'text/javascript' :
+      'text/html'
     res.writeHead(200, { 'Content-Type': mime, })
     res.end(data)
   } catch (e) {
