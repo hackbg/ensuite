1b1948756c4dc2576d799b79e74b78df2aeea465
Adam Avramov <adam@hack.bg>
2023-10-19 02:35:24 +0300

    feat(1.5.0-rc.0): add test selection fns and type definitions

---
+++ ensuite.d.ts
@@ -0,0 +1,9 @@
+export function main (root?: string, ...specs: any[])
+
+export function testEntrypoint (url: URL, tests: Record<string, Function>): typeof tests
+
+export function pickTest (tests: Record<string, Function>, picked?: string): never
+
+export function testAll (tests: Record<string, Function>): Promise<unknown[]>
+
+export function testSuite (path: string): Promise<() => Promise<never>>
--- ensuite.mjs
+++ ensuite.mjs
@@ -1,7 +1,10 @@
 import why from 'why-is-node-still-running'
 import { runSpec } from '@hackbg/spec'
-import { resolve} from 'path'
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 main(...process.argv.slice(2))
+
 export async function main (root = process.cwd(), ...specs) {
   // If tests don't exit, press "?" to see why
   if (process.stdin.setRawMode) process.stdin.setRawMode(true)
@@ -21,4 +24,37 @@ export async function main (root = process.cwd(), ...specs) {
   await runSpec(suite.default, specs)
   console.log('Tests done.')
   process.stdin.pause()
+}
+
+export function testEntrypoint (url, tests) {
+  const entrypoint = resolve(process.argv[2])
+  const mainScript = resolve(fileURLToPath(url))
+  if (entrypoint === mainScript) return pickTest(tests)
+  return tests
+}
+
+export async function pickTest (tests, picked = process.argv[3]) {
+  if (picked === 'all') return testAll(tests)
+  const test = tests[picked]
+  if (test) return await test()
+  console.log('\nSpecify suite to run:')
+  console.log(`  all`)
+  for (const test of Object.keys(tests)) {
+    console.log(`  ${test}`)
+  }
+  console.log()
+  process.exit(1)
+}
+
+export async function testAll (tests) {
+  const runs = Object.values(tests).map(test=>test())
+  return await Promise.all(runs)
+}
+
+export function testSuite (path) {
+  return async () => {
+    console.log('Testing:', path)
+    const { default: suite } = await import(path)
+    return pickTest(suite, process.argv[4])
+  }
 }
