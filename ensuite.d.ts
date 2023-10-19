42ab1cf50fff8dfaab2cbc3e83f1b47148acd0ec
Adam Avramov <adam@hack.bg>
2023-10-19 08:24:23 +0300

    fix(types): URL->string|url in testEntrypoint

--- ensuite.d.ts
+++ ensuite.d.ts
@@ -1,6 +1,6 @@
 export function main (root?: string, ...specs: any[])
 
-export function testEntrypoint (url: URL, tests: Record<string, Function>): typeof tests
+export function testEntrypoint (url: string|URL, tests: Record<string, Function>): typeof tests
 
 export function pickTest (tests: Record<string, Function>, picked?: string): never
 
