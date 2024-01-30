954106e0f904bcebf68d5ddcd2e123b4a6abbef5
Adam A <adam@hack.bg>
2024-01-30 20:45:54 +0200

    wip: feat(ensuite): run tests sequentially by default

--- ensuite.mjs
+++ ensuite.mjs
@@ -7,11 +7,13 @@ const console = new Console('@hackbg/ensuite')
 
 export default async function main (root = process.cwd(), ...specs) {
 
-  // Handle exceptions and rejections
+  // Handle exceptions
   process.on('uncaughtExceptionMonitor', (error, origin) => {
     console.warn('Uncaught exception:', { error, origin })
     throw error
   })
+
+  // Handle rejections
   process.on('unhandledRejection', (reason, promise) => {
     console.warn('Unhandled promise rejection:', { reason, promise })
     throw reason
@@ -19,7 +21,9 @@ export default async function main (root = process.cwd(), ...specs) {
 
   // If tests don't exit, press "?" to see why
   if (process.env.ENSUITE_WHY) {
-    if (process.stdin.setRawMode) process.stdin.setRawMode(true)
+    if (process.stdin.setRawMode) {
+      process.stdin.setRawMode(true)
+    }
     process.stdin.resume()
     process.stdin.setEncoding('utf8')
     process.stdin.on('data', key => {
@@ -34,14 +38,16 @@ export default async function main (root = process.cwd(), ...specs) {
 
   // Load and run tests
   const index = resolve(process.cwd(), root)
-  const { default: suite } = await import(index)
-  if (resolve(process.argv[2]) === index) {
-    await new Promise((resolve, reject)=>{
-      setImmediate(()=>Promise.resolve(suite.run({
-        argv: process.argv.slice(3)
-      })).then(resolve, reject))
-    })
+  const suite = (await import(index)).default
+  const argv  = process.argv.slice(3)
+  let parallel = false
+  if (argv[0] === '--parallel') {
+    parallel = true
+    argv.shift()
   }
+  await new Promise((resolve, reject)=>{
+    setImmediate(()=>Promise.resolve(suite.run({ argv, parallel })).then(resolve, reject))
+  })
 
   console.log('Tests done.')
   process.exit(0)
@@ -66,14 +72,14 @@ export class Suite {
     this.tests = new Map(tests)
   }
 
-  async run ({ argv = [], all = false } = {}) {
+  async run ({ argv = [], all = false, parallel = false } = {}) {
     //console.info(`Selection: '${argv.join(' ')}'`)
     argv = [...argv]
     if (this.tests.length === 0) {
       throw new Error('no tests static')
     }
     if (argv.length === 0) {
-      return this.selectTest()
+      return this.printUsage()
     }
     if (argv[0] === 'all') {
       return this.runAll()
@@ -88,7 +94,7 @@ export class Suite {
       }
       if (!suite.tests.has(name)) {
         console.info(`Not found: ${bold(name)}`)
-        return suite.selectTest()
+        return suite.printUsage()
       }
       let selected = suite.tests.get(name)
       if (selected instanceof Suite) {
@@ -137,20 +143,31 @@ export class Suite {
       }
       throw new Error(`${name} is not a Function or Suite`)
     }
-    return suite.selectTest()
+    return suite.printUsage()
   }
 
-  runAll () {
+  async runAll ({ parallel = false } = {}) {
     const names = [...this.tests.keys()]
-    console.info(`Running: all of ${names.map(x=>bold(x)).join(', ')}`)
-    return Promise.all(names.map(async name=>{
-      return this.run({ argv: [name], all: true })
-    }))
+    if (parallel) {
+      console.info(`Running (in parallel) all of: ${names.map(x=>bold(x)).join(', ')}`)
+      return await Promise.all(names.map(name=>{
+        return this.run({ argv: [name], all: true, parallel })
+      }))
+    } else {
+      console.info(`Running (sequentially) all of: ${names.map(x=>bold(x)).join(', ')}`)
+      for (const name of names) {
+        console.info(`Running: ${bold(name)}`)
+        await this.run({ argv: [name], all: true, parallel })
+      }
+    }
   }
 
-  selectTest () {
-    console.info('Please specify a test suite to run:')
-    console.info(`  all`)
+  printUsage () {
+    console
+      .info('Usage:')
+      .info('  ensuite [--parallel] testName [testName...]')
+      .info('Please specify a test suite to run:')
+      .info(`  all`)
     for (const name of this.tests.keys()) {
       console.info(`  ${name}`)
     }
